89bb3ad3f5c41db856ce3a9b524da1a931cb0221
linkaihan98 <19927459677@163.com>
2021-11-03 15:22:05 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,40 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1635921324178_3272';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // 编写 sequelize 配置
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'egg_blog',
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
